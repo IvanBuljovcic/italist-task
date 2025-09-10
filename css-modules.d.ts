f83d0d7a0bc9b86f4af700925fb3a50bffb34701
Ivan Buljovčić <ivan.buljovcic@gmail.com>
2025-09-10 21:46:18 +0200

    chore: linting

--- css-modules.d.ts
+++ css-modules.d.ts
@@ -1,20 +1,20 @@
 // css-modules.d.ts
-declare module '*.module.css' {
+declare module "*.module.css" {
 	const classes: { [key: string]: string };
 	export default classes;
 }
 
-declare module '*.module.scss' {
+declare module "*.module.scss" {
 	const classes: { [key: string]: string };
 	export default classes;
 }
 
-declare module '*.module.sass' {
+declare module "*.module.sass" {
 	const classes: { [key: string]: string };
 	export default classes;
 }
 
-declare module '*.module.less' {
+declare module "*.module.less" {
 	const classes: { [key: string]: string };
 	export default classes;
 }
